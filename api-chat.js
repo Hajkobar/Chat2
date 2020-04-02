0ff0ca7ee6e88100b203f55edf20b88306923a2a
Bara <hajkobar1@duhovkagymnazium.cz>
2020-04-02 15:35:13 +0200

    v tom lepším případě mi tohle nerozhodí můj projekt. Pokud ano, budu naštvaná...

--- api-chat.js
+++ api-chat.js
@@ -6,6 +6,10 @@ exports.apiChat = function (req, res, obj) {
     if (req.pathname.endsWith("/list")) {
         obj.messages = msgs;
     } else if (req.pathname.endsWith("/add")) {
-        msgs.push("test");
+        let obj = {};
+        obj.message = "";
+        obj.time = new Date();
+        msgs.push(obj);
     }
+
 }
