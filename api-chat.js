17cbe18ca1b54cd49c3e7b769c42aba61cdeca72
Barbora Hájková <hajkobar1@duhovkagymnazium.cz>
2020-05-14 12:39:39 +0200

    saving messages

--- api-chat.js
+++ api-chat.js
@@ -1,6 +1,13 @@
 const dateFormat = require('dateformat');
-
+const fs = require(`fs`);
 let msgs = [];
+
+
+const MSGS_FILE = "chat.json";
+if (fs.existsSync(MSGS_FILE)) {
+    msgs = JSON.parse(fs.readFileSync(MSGS_FILE));
+}
+
 
 exports.apiChat = function (req, res, obj) {
     if (req.pathname.endsWith("/list")) {
@@ -12,6 +19,7 @@ exports.apiChat = function (req, res, obj) {
         obj.time = dateFormat(new Date(), "dd.mm.yyyy HH:MM:ss");
         obj.nickname = req.parameters.nick;
         msgs.push(obj);
+        fs.writeFileSync(MSGS_FILE, JSON.stringify(msgs,null,2))
     }
 
 };
